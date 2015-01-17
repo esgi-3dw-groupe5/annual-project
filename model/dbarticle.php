6f956a072ebdbdf2f8d53433d69b9d807a254d21
athorel <alexis.thorel@gmail.com>
2015-01-17 16:10:23 +0100

    [feature] delete/update article
    FIXME :
    - test
    - access to id_category ( function delete )
    - view for update article

--- model/dbarticle.php
+++ model/dbarticle.php
@@ -26,3 +26,20 @@ function db_create_article($link, $title, $content, $id_category){
 		die();
 	}
 }
+
+function db_delete_article($link, $value){
+	$req = $link -> prepare("DELETE FROM pp_article WHERE id = :value");
+	$req->execute(array(
+		':value' => $value
+	));
+
+function db_update_article($link,$title,$content,$value){
+	$req = $link -> prepare("UPDATE FROM pp_article SET title   = :title,
+														content = :content WHERE id = :value");
+	$req->execute(array(
+		':title'   => $title,
+		':content' => $content,
+		':value'   => $value
+	));
+}
+}
